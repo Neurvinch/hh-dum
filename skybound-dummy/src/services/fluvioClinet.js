ff077468bdab0d6b4b8714caee9627a14ae1cb4b
Naveen pandian <npandian515@gmail.com>
2025-04-17 14:33:49 +0530

    feat: implement sendGameEvent function to handle game events

--- skybound-dummy/src/services/fluvioClinet.js
+++ skybound-dummy/src/services/fluvioClinet.js
@@ -1,1 +1,12 @@
 import  Fluvio from "@fluvio/client"
+
+let  producer;
+
+export async function sendGameEvent(topic, event) {
+    if(!producer) {
+        const fluvio = await Fluvio.connect();
+        producer = await fluvio.topicProducer(topic);
+    }
+
+    await producer.send({ key : event.playerId, value : JSON.stringify(event)})
+} 
